a25bfded7fc47dd56787e5afaf644c6607f2fc1a
Hai Zhu <hz1778@nyu.edu>
2022-04-29 16:19:54 -0400

    data import and preprocess

--- main.py
+++ main.py
@@ -16,3 +16,36 @@ random.seed(18402254)
 # Load data
 data = pd.read_csv('musicData.csv')
 
+#missing data
+missing = data.isnull().sum()
+print(missing)
+
+# imputing missing numeric data
+data['instance_id'] = data['instance_id'].fillna(data['instance_id'].mean())
+data['popularity'] = data['popularity'].fillna(data['popularity'].mean())
+data['acousticness'] = data['acousticness'].fillna(data['acousticness'].mean())
+data['danceability'] = data['danceability'].fillna(data['danceability'].mean())
+data['duration_ms'] = data['duration_ms'].fillna(data['duration_ms'].mean())
+data['energy'] = data['energy'].fillna(data['energy'].mean())
+data['instrumentalness'] = data['instrumentalness'].fillna(data['instrumentalness'].mean())
+data['liveness'] = data['liveness'].fillna(data['liveness'].mean())
+data['loudness'] = data['loudness'].fillna(data['loudness'].mean())
+data['speechiness'] = data['speechiness'].fillna(data['speechiness'].mean())
+data['valence'] = data['valence'].fillna(data['valence'].mean())
+
+# delete missing data
+data = data.dropna()
+missing = data.isnull().sum()
+
+# replace categorical data with numerical data
+key = data['key'].unique()
+data['key'] = data['key'].replace(key, range(len(key)))
+data['mode'] = data['mode'].replace(['minor', 'major'], [0, 1])
+music_genere = data['music_genre'].unique()
+data['music_genre'] = data['music_genre'].replace(music_genere, range(len(music_genere)))
+
+# drop unnecessary columns
+preprocessed_data = data.drop('artist_name', axis=1)
+preprocessed_data = preprocessed_data.drop('track_name', axis=1)
+preprocessed_data = preprocessed_data.drop('obtained_date', axis=1)
+print(preprocessed_data.head())
